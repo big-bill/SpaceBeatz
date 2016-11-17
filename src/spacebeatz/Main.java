cd614f2a71b6b0282d997e74a47c9af04616e033
big-bill <Billy-Matthews@outlook.com>
2016-11-17 14:14:45 -0600

    first commit

---
+++ src/spacebeatz/Main.java
@@ -0,0 +1,29 @@
+package spacebeatz;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			Parent parent = FXMLLoader.load(getClass().getResource("MainMenu.fxml"));
+			Scene scene = new Scene(parent);
+			primaryStage.setTitle("Space Beatz");
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setResizable(false);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
---
+++ src/spacebeatz/MainMenu.java
@@ -0,0 +1,125 @@
+package spacebeatz;
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
+
+import javafx.animation.Timeline;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaPlayer.Status;
+import javafx.stage.FileChooser;
+import javafx.stage.Window;
+
+
+public class MainMenu {
+
+    @FXML
+    private AnchorPane anchorPane;
+
+    @FXML
+    private ImageView spaceBeatzLogo;
+
+    @FXML
+    private Label chooseSongLabel;
+
+    @FXML
+    private Label chosenSong;
+
+    @FXML
+    private Button browseButton;
+
+    @FXML
+    private Button playButton;
+    
+    private Media audioFile;
+    private MediaPlayer mediaPlayer;
+    
+    private Media backgroundMusicAudio;
+    private MediaPlayer backgroundMusicPlayer;
+    
+    @FXML
+    public void initialize() {
+    	audioFile = null;
+    	mediaPlayer = null;
+    	
+    	try {
+    	/*	
+    	 *  Music Source:
+    	 * 		Voltaic Kevin MacLeod (incompetech.com)
+    	 * 		Licensed under Creative Commons: By Attribution 3.0 License
+    	 * 		http://creativecommons.org/licenses/by/3.0/
+    	 */    		
+    		File file = new File("res/backgroundmusic.mp3");
+	    	URI uri = file.toURI();
+	    	URL url = uri.toURL();
+	
+	    	
+	    	backgroundMusicAudio = new Media(url.toString());
+	    	backgroundMusicPlayer = new MediaPlayer(backgroundMusicAudio);
+	    	backgroundMusicPlayer.setVolume(0.2);
+	    	backgroundMusicPlayer.setCycleCount(Timeline.INDEFINITE);
+	    	backgroundMusicPlayer.play();
+    	
+    	} 
+    	catch (MalformedURLException | NullPointerException | IllegalArgumentException err) {
+    		err.printStackTrace();
+    	}
+    	catch (Exception e) {
+    		e.printStackTrace();
+    	}
+    	playButton.setDisable(true);    	
+    }
+
+    //-----------------------------------------------------------------------------------------------------------
+    
+    public void browseButtonListener() {
+    	// Open up a FileChooser and let the user choose a track to play
+    	FileChooser fileChooser = new FileChooser();
+    	// Filter only mp3 files
+        FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("MP3 files (*.mp3)", "*.mp3");
+        fileChooser.getExtensionFilters().add(extFilter);
+        Window stage = null;  // TODO: Is this needed? Couldn't figure out how to implement a FileChooser without a stage
+		File fileChosen = fileChooser.showOpenDialog(stage);
+		if (fileChosen != null) {  
+			chosenSong.setText(fileChosen.getAbsolutePath()); 
+			playButton.setDisable(false); 
+		}
+    }
+    
+    //-----------------------------------------------------------------------------------------------------------
+  
+    public void playButtonListener() {
+    	// For now this will play music
+    	// TODO: Send chosen file to game file to play
+    	try {
+    		// If music is currently being played, stop it
+    		if(mediaPlayer != null) mediaPlayer.stop();
+    		
+    		File musicFile = new File(chosenSong.getText());
+    		URI uri = musicFile.toURI();
+    		URL url = uri.toURL();
+    
+    		
+    		audioFile = new Media(url.toString());
+    		mediaPlayer = new MediaPlayer(audioFile);
+    		
+    		// Since the media player successfully accessed the audio file, we stop playing the menu music
+    		if(backgroundMusicPlayer.getStatus() != Status.STOPPED) backgroundMusicPlayer.stop();
+    		
+    		mediaPlayer.play();
+    	}
+    	catch (NullPointerException | MalformedURLException | IllegalArgumentException e1) {
+    		System.err.println("No song found!");
+    	}
+    	catch(Exception e2) {
+    		System.err.print(e2.getMessage());
+    	}
+    }
+    
+}
