8ba93beda3d1952f4eeed8f04c88890afbc2efb6
Robert <Robert505189@gmail.com>
2016-11-27 17:33:31 -0600

    Built Hud class

--- src/spacebeatzgame/Hud.java
+++ src/spacebeatzgame/Hud.java
@@ -1,60 +1,144 @@
-/*
- *  Author:       Robert Munshower
- *  File Name:    Hud
- *  Package:      spacebeatzgame
- *  Project Name: SpaceBeatz 
- *  Created:      Nov 27, 2016
- *  Last Modified: 
- */
-
 package spacebeatzgame;
 
-import java.io.File;
-import java.net.MalformedURLException;
-import java.net.URI;
-import java.net.URL;
+import javafx.scene.Group;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 
 /**
- *About Hud
+ *About Hud Class
  * 
  * 
  */
 public class Hud {
-    private Rectangle hudRect = new Rectangle(600,100);
-    private Image shipImage;
-    private final double WIDTH = 10;
-    private final double HEIGHT = 10;
     
+    /**
+     * hudRect represents the heads up display of the game and contains
+     * pertinent game statistics
+     */
+    private Rectangle hudRect;
+    
+    /**
+     * Hud Color
+     */
+    private final Color HUD_COLOR = Color.SKYBLUE;
+    
+    /**
+     * Opacity of HUD
+     */
+    private final double HUD_OPACITY = .3;
+    
+    /**
+     * WIDTH represents hud width
+     */
+    private final double WIDTH = 400;
+    
+    /**
+     * HEIGHT represents hud height
+     */
+    private final double HEIGHT = 100;
+    
+    /**
+     * Vertical diameter of the arc at the four corners of the rectangle.
+     */
+    private final double ARC_HEIGHT = 20;
+    
+    /**
+     * Horizontal diameter of the arc at the four corners of the rectangle
+     */
+    private final double ARC_WIDTH = 20;
+    
+    /**
+     * Holds elapsed time of game
+     */
+    private Label etLabel;
+    
+    /**
+     * Holds current game score
+     */
+    private Label scoreLabel;
+    
+    /**
+     * Holds String for Elapsed Time
+     */
+    private final String ET_STRING = "Elapsed Time: ";
+    
+    /**
+     * Holds String for Score
+     */
+    private final String SCORE_STRING = "Score: ";
+    
+    /**
+     * Holds game font
+     */
+    private final Font GAME_FONT = Font.font("Agency FB Bold",FontWeight.BOLD,14);
+    
+    /**
+     * Holds statistics for hud with a 12 pixel spacing.
+     */
+    private HBox statBox = new HBox(12);
+
+    /**
+     * Hud x position.
+     */
+    protected double positionX;
+
+    /**
+     * Hud y position.
+     */
+    protected double positionY;
+    
+    
+    
+    
+
     /**
      * Constructor for Hud
      */
-    Hud(){
+    Hud() {
+
+        //Set Attributes of the hud
+        hudRect = new Rectangle(WIDTH, HEIGHT);
+        hudRect.setFill(HUD_COLOR);
+        hudRect.setArcHeight(ARC_HEIGHT);
+        hudRect.setArcWidth(ARC_WIDTH);
+        hudRect.setOpacity(HUD_OPACITY);
         
-//        hudRect.setFill(Color.SKYBLUE);
-//                hudRect.setArcHeight(20);
-//                hudRect.setArcWidth(20);
-//                hudRect.setOpacity(.3);
-//                
-//                HBox livesBox = new HBox();
-//                Image[] livesArray;
-//                try {
-//			File shipFile = new File("src/spacebeatzgame/res/ship.png");
-//			URI uri = shipFile.toURI();
-//			URL url = uri.toURL();
-//			shipImage = new Image(url.toString(),WIDTH,HEIGHT, preserveRatio, smooth);
-//			
-//		} 
-//		catch(IllegalArgumentException | MalformedURLException e) {
-//			e.printStackTrace();
-//		}
-//                for (int lifeCounter = 0; lifeCounter < 3; lifeCounter++){
-//                    livesArray = new Image[lifeCounter](""); 
-//                }
-    
+        //Set Attributes of labels
+        etLabel.setText(ET_STRING + "--:--");
+        etLabel.setFont(GAME_FONT);
+        scoreLabel.setText(SCORE_STRING + "000,000");
+        scoreLabel.setFont(GAME_FONT);
+        
+        
+        //add children to statBox
+        statBox.getChildren().addAll(etLabel,scoreLabel);
+        statBox.setFocusTraversable(false);
+        
+
     }
+    /**
+     * Sets position of hud.
+     * @param xPos x coordinate position
+     * @param yPos y coordinate position
+     */
+    public void setHudPos(double xPos, double yPos){
+        hudRect.setX(xPos);
+        hudRect.setY(yPos);
+    }
+    
+    /**
+     * Get the hud
+     * 
+     * @return the rectangle node containing formated hud.
+     */
+    public Rectangle getHud(){
+        return hudRect;
+    }
+
 
 }
